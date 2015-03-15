b3c352f6a366cba590383aaf78a336d780a74f14
yuanzm <1229084233@qq.com>
2015-03-16 00:43:36 +0800

    test

---
+++ README.md
@@ -0,0 +1,15 @@
+ExpressJS boilerplate for Openshift
+===============================
+A JS stack using MongoDB, ExpressJS, Bootstrap 2.3, NodeJS for Openshift
+
+Packages
+-
+- ExpressJS version: 4.2
+- Bootstrap 2.3
+
+OpenShift Resources
+-
+- [Set-up SSL certificate with OpenShift](http://cloudhostingsource.com/setup-ssl-certificate-openshift/)
+- [OpenShift Environmental Variables](https://developers.openshift.com/en/managing-environment-variables.html)
+- [NodeJS Environmental Variables](https://developers.openshift.com/en/node-js-environment-variables.html)
+- [MongoDB Environmental Variables](https://developers.openshift.com/en/databases-mongodb.html)
--- server.js
+++ server.js
@@ -1,134 +1,34 @@
-#!/usr/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
-var routes  = require('./routes');
-var user    = require('./routes/user');
-var path    = require('path');
+#!/bin/env node
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+var app = express();
 
-    //  Scope.
-    var self = this;
+app.configure(function(){
+  app.locals.pretty = true;
+  app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+  app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  //Favicon
+//  app.use(express.favicon( __dirname + "/public/favicon.ico", { maxAge: 2592000000 }));
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+app.get('/', routes.index);
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_INTERNAL_IP;
-        self.port      = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_INTERNAL_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
-
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.app = express();
-
-        self.app.configure(function() {
-            self.app.set('views', __dirname + '/views');
-            self.app.set('view engine', 'ejs');
-            self.app.use(express.favicon());
-            self.app.use(express.logger('dev'));
-            self.app.use(express.bodyParser());
-            self.app.use(express.methodOverride());
-            self.app.use(express.cookieParser('Op0mfIhs01Uk1t14G4paudaSPrbDswMXjKcwOZDs44ey5pTvESFOerCh4urTVZJ0'));
-            self.app.use(express.session());
-            self.app.use(self.app.router);
-            self.app.use(require('stylus').middleware(__dirname + '/public'));
-            self.app.use(express.static(path.join(__dirname, 'public')));
-        });
-        //  Add handlers for the app (from the routes).
-        self.app.get('/', routes.index);
-        self.app.get('/users', user.list);
-    }
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
+http.createServer(app).listen(app.get('port'), app.get('ipaddress'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
